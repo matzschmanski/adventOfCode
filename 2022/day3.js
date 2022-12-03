e0ad5aba54e152145c576ec0fd4d1c0a91db3246
Matthias Geiger <matthias.geiger@arvato-scs.com>
2022-12-03 07:12:17 +0100

    day 3 beautifulllll

--- 2022/day3.js
+++ 2022/day3.js
@@ -1,80 +1,36 @@
 import { readFile } from './util/util.js';
 
 var input = readFile('/Users/geig006/Development/projects/priv/adventOfCode/2022/input/day3_1');
-
+var startTime = performance.now()
 var charsLower ='abcdefghijklmnopqrstuvwxyz';
 var charsUpper ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var inBoth =[];
 
 var input = input.split(/\r?\n/);
 
+var sum =0;
+var chunkSize=3
+for (let i = 0; i < input.length; i += chunkSize) {
+    const chunk = input.slice(i, i + chunkSize);
+    const letter = chunk[0].split('').filter(value => chunk[1].split('').includes(value)).filter(value => chunk[2].split('').includes(value));
+    inBoth.push(letter[0]);
+}
 
 
-while( input.length>0){
-    const firstCompartment = input[0].split('');
-const secondCompartment = input[1].split('');
-const thirdCompartment = input[2].split('');
-for (var i = 0; i < firstCompartment.length; i++) { 
-    if(secondCompartment.includes(firstCompartment[i])&& thirdCompartment.includes(firstCompartment[i])){
-         inBoth.push(firstCompartment[i]);
-         break;
-    }
-}
-console.log(input.length);
-input= input.slice(3);
-}
-  var sum =0;
+// while( input.length>0){
+// const letter = input[0].split('').filter(value => input[1].split('').includes(value)).filter(value => input[2].split('').includes(value));
+// inBoth.push(letter[0]);
+// input= input.slice(3);
+// }
+
   for (var i = 0; i < inBoth.length; i++) { 
   if(inBoth[i] == inBoth[i].toUpperCase()){
-    //upper
-    var index=charsUpper.indexOf(inBoth[i])+27;
-    console.log('code',inBoth[i] + ' ' + index);
-    sum +=index;
+    sum +=charsUpper.indexOf(inBoth[i])+27;
   } else if(inBoth[i] == inBoth[i].toLowerCase()){
-    //lower
-    var index=charsLower.indexOf(inBoth[i])+1;
-    console.log('code',inBoth[i] + ' ' + index);
-    sum +=index;
+    sum +=charsLower.indexOf(inBoth[i])+1;
   }
-  console.log('sum',sum);
+  
 }
-  
-
-  
-
-
-  //console.log(highest.reduce((partialSum, a) => partialSum + a, 0));
-
-  function calculateMatchPoints(inputElv, inputPlayer){
-    const ROCK_ELV = "A";
-    const SCISSORS_ELV = "C";
-    const PAPER_ELV = "B";
-    const DRAW = "Y";
-    const LOOSE = "X";
-    if(ROCK_ELV === inputElv){
-        if(LOOSE === inputPlayer){
-            return 3;
-        }else if (DRAW === inputPlayer){
-            return 4;
-        }else {
-            return 8;
-        }
-    }else if(PAPER_ELV === inputElv){
-        if(LOOSE === inputPlayer){
-            return 1;
-        }else if (DRAW === inputPlayer){
-            return 5;
-        }else {
-            return 9;
-        }
-
-    }else if (SCISSORS_ELV === inputElv){
-        if(LOOSE === inputPlayer){
-            return 2;
-        }else if (DRAW === inputPlayer){
-            return 6;
-        }else {
-            return 7;
-        }
-    }
-  }
+console.log('sum',sum);
+var endTime = performance.now()
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
