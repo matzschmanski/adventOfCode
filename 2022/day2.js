6d5e9712a204cb6d51819eb24ca1cfd74ee854c4
Matthias Geiger <matthias.geiger@arvato-scs.com>
2022-12-03 05:58:41 +0100

    current

--- 2022/day2.js
+++ 2022/day2.js
@@ -3,22 +3,11 @@ import { readFile } from './util/util.js';
 var input = readFile('/Users/geig006/Development/projects/priv/adventOfCode/2022/input/day2_1');
 
 var highest =[];
-var currentElv =0;
 input.split(/\r?\n/).forEach(line =>  {
     const lineValues = line.split(' ');
     highest.push(calculateMatchPoints(lineValues[0],lineValues[1]))
   });
   console.log(highest.reduce((partialSum, a) => partialSum + a, 0));
-
-
-  /**
-   * A for Rock, B for Paper, and C for Scissors.
-   * X means you need to lose, 
-   * Y means you need to end the round in a draw, 
-   * and Z means you need to win. Good luck!"
-   * 1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round 
-   * (0 if you lost, 3 if the round was a draw, and 6 if you won)
-   */
 
   function calculateMatchPoints(inputElv, inputPlayer){
     const ROCK_ELV = "A";
