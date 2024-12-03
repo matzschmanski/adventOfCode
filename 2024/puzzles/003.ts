35bd2933e481326d6b656a2c010c9c5aa0e9b326
Matthias Geiger <matthias.geiger@arvato-scs.com>
2024-12-03 10:12:54 +0100

    day 3 part 2

--- 2024/puzzles/003.ts
+++ 2024/puzzles/003.ts
@@ -1,27 +1,29 @@
-import { readTxtFile } from '../snippets/read_input';
+import { readTxtFile, readTxtFileLines } from '../snippets/read_input';
 
 
 
 
 export const main = (): string => {
     var input = readTxtFile("003.txt");
-    // regex for the input mul\(^\)*)
-    // var input = readTxtFileLines("003.txt");
-
-    var matches;
+    //replace all new lines
+    input = input.replace(/\n/g, ',');
     var sum = 0;
-    //var regexp = new RegExp('mul\((^\)*)');
-    var regexp = new RegExp('mul\\((\\d+),(\\d+)\\)', 'g');
-    console.log('regex', regexp)
-    do {
-        matches = regexp.exec(input);
-        if (matches) {
-            
-            console.log(matches[1]);
-            console.log(matches[2]);
-            sum += parseInt(matches[1]) * parseInt(matches[2]);
-        }
-    } while (matches);
+    input = input.replace(/don't\(\)[\s\S]*?do\(\)/g, '');
+    input = input.replace(/don't\(.*/, '');
+        console.log(input);
+        // regex for the input mul\(^\)*)
+        // var input = readTxtFileLines("003.txt");
+
+        var matches;
+
+        //var regexp = new RegExp('mul\((^\)*)');
+        var regexp = new RegExp('mul\\((\\d+),(\\d+)\\)', 'g');
+        do {
+            matches = regexp.exec(input);
+            if (matches) {
+                sum += parseInt(matches[1]) * parseInt(matches[2]);
+            }
+        } while (matches);
 
     return sum.toString();
 }
